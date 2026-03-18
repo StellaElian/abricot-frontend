cb54699e3dbd76d8b62c20df785e760b1aea6303
Stella Elian <elian_stella@yahoo.com>
2026-03-18 14:39:16 +0100

    Création de la page et du dossier dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-orange-500">Mon Tableau de Bord</h1>
+      <p>Ici, on verra le résumé des tâches.</p>
+    </div>
+  );
+}
