4249d2a1b2588f935c873a8236cf58028be2984c
Stella Elian <elian_stella@yahoo.com>
2026-04-12 01:49:22 +0200

    Suppression fichier test du début

--- src/app/tasks/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-slate-50 flex items-center justify-center">
-      <h1 className="text-4x1 font-bold text-orange-500">
-        Bienvenue sur Abricot 🍑
-      </h1>
-    </div>
-  );
-}
